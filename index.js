49ff287feba999ac251e59e12e4f05075cc2f0b0
Eduardo Penco <pencoeduardo@gmail.com>
2022-06-29 14:14:57 -0300

    modificando ejercicio

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
-//imprime en consola los numeros impares de 1 a 100
-
-for (let i = 0; i <= 100; i++) {
+//imprime por consola los numeros impares de 1 a 100
+let entrada = prompt ("quiere ver los numeros impares del 1 al 100, responda si o no").toLowerCase();
+if (entrada == "si"){
+    for (let i = 0; i <= 100; i++) {
     
-    if (i % 2 !== 0) {
+      if (i % 2 !== 0) {
         console.log(i);
     }
+}
+}
+ else if (entrada=="no"){
+    console.log("gracias");
+}
+else {
+    alert("no entiendo su respuesta");
 }
