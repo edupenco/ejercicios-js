1344d39f597dc4f1b6b36fa06121e194a4f3ac20
Eduardo Penco <pencoeduardo@gmail.com>
2022-07-08 15:58:14 -0300

    agregando desafio arrays

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
 //imprime por consola los numeros impares de 1 a 100
-let entrada = prompt ("quiere ver los numeros impares del 1 al 100, responda si o no").toLowerCase();
-if (entrada == "si"){
-    for (let i = 0; i <= 100; i++) {
+// let entrada = prompt ("quiere ver los numeros impares del 1 al 100, responda si o no").toLowerCase();
+// if (entrada == "si"){
+//     for (let i = 0; i <= 100; i++) {
     
-      if (i % 2 !== 0) {
-        console.log(i);
-    }
+//       if (i % 2 !== 0) {
+//         console.log(i);
+//     }
+// }
+// }
+//  else if (entrada=="no"){
+//     console.log("gracias");
+// }
+// else {
+//     alert("no entiendo su respuesta");
+// }
+
+
+//Calcula el resultado de la suma de los numeros del 1 al 10 en un array.
+  //  [1,2,3,4,5,6,7,8,9,10]
+
+const NUMEROS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let sumaDeNumeros = 0;
+
+for (let i = 0; i < NUMEROS.length; i++) {
+    sumaDeNumeros += NUMEROS[i];
 }
-}
- else if (entrada=="no"){
-    console.log("gracias");
-}
-else {
-    alert("no entiendo su respuesta");
-}
+
+console.log(`La suma de números del array es: ${sumaDeNumeros}.`);
